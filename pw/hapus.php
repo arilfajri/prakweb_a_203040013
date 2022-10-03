4beeb0e49fd81d30c20d1e90b6de86899457b21f
arilfajri <arielmunchen60@gmail.com>
2022-10-03 21:56:59 +0700

    memodifikasi alert di file hapus.php

--- pw/hapus.php
+++ pw/hapus.php
@@ -38,14 +38,14 @@ function hapus($id)
 if (hapus($id) > 0) {
   echo "
             <script>
-                alert('data berhasil dihapus!');
+                alert('data berhasil dihapuss!');
                 document.location.href = 'index.php';
             </script>
         ";
 } else {
   echo "
             <script>
-                alert('data gagal ditambahkan!');
+                alert('data gagal ditambahkann!');
                 document.location.href = 'index.php';
             </script>
         ";
